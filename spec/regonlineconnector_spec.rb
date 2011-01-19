56592999b34cee823a18ac4889ae321cbf09774f
Brian Roberg <robergb@dm.org>
2011-01-19 14:55:17 -0500

    Ported tests previously written as unit tests.

--- spec/regonlineconnector_spec.rb
+++ spec/regonlineconnector_spec.rb
@@ -1,7 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RegonlineConnector" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  
+  it "shouldn't instantiate without account info" do
+    lambda { roc = RegonlineConnector.new }.should raise_exception(ArgumentError)
+  end
+  
+  it "should not give access to account_id" do
+    roc = RegonlineConnector.new(100, 'joeuser', 'password')
+    lambda { roc.account_id }.should raise_exception(NoMethodError)
+  end
+  
+  it "should not give access to username" do
+    roc = RegonlineConnector.new(100, 'joeuser', 'password')
+    lambda { roc.username }.should raise_exception(NoMethodError)
+  end
+  
+  it "should not give access to password" do
+    roc = RegonlineConnector.new(100, 'joeuser', 'password')
+    lambda { roc.password }.should raise_exception(NoMethodError)
+  end
+  
+  it "should not authenticate with bogus credentials" do
+    roc = RegonlineConnector.new(100, 'joeuser', 'password')
+    roc.authenticate.should be_an_instance_of(FalseClass)
   end
 end
