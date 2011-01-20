42552848433bf7ce0f83f53562f2f02081e3a1fb
Brian Roberg <robergb@dm.org>
2011-01-20 14:39:35 -0500

    Implemented the \"event\" method and added basic RDoc comments for each method and the class.

--- lib/regonlineconnector.rb
+++ lib/regonlineconnector.rb
@@ -2,24 +2,31 @@ require 'regonlineconnector/client'
 require 'regonlineconnector/parser'
 require 'regonlineconnector/error'
 
+# Class providing a gateway to other classes that connect to RegOnline.
 class RegonlineConnector
   def initialize(account_id, username, password)
     @client  = RegonlineConnector::Client.new(account_id, username, password)
     @parser  = RegonlineConnector::Parser.new
   end
   
+  # Returns boolean indicating whether valid login credentials have
+  # been supplied.
   def authenticate
     @authenticate = @client.authenticate
   end
   
+  # Returns hashed data from RegOnline's getEvents.byAccountID method.
   def events
     @parser.parse_events(@client.getEvents.byAccountID)
   end
   
+  # Returns hashed data from RegOnline's getEvents.byAccountIDEventID method.
   def event(event_id)
-    raise NotImplementedError    
+    @parser.parse_events(@client.getEvents.byAccountIDEventID(event_id))
   end
   
+  # Returns hashed data from RegOnline's getEvents.byAccountIDWithFilters
+  # method. Not yet implemented.
   def filtered_events(filter)
     raise NotImplementedError
   end
