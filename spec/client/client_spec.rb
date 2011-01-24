83a833972f954161f1e9f5418f1c883dac85360b
Adam Focht <fochta@dm.org>
2011-01-24 17:04:01 -0500

    Set up rspec directories in preparation for deeper tests.

---
+++ spec/client/client_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Client" do
+  describe "without credentials" do
+    it "shouldn't instantiate" do
+      lambda { roc_client = RegonlineConnector::Client.new }.should raise_exception(ArgumentError)
+    end
+  end
+end
