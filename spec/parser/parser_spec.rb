3766bf227e3996ea78108ff64798af79e6e01762
Adam Focht <fochta@dm.org>
2011-01-25 11:19:48 -0500

    Added Parser spec.

--- spec/parser/parser_spec.rb
+++ spec/parser/parser_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Client" do
+describe "Parser" do
   describe "without credentials" do
     it "shouldn't instantiate" do
       lambda { roc_client = RegonlineConnector::Client.new }.should raise_exception(ArgumentError)
