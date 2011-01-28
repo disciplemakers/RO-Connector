80d8db7d00dc36e8701a445613efed55358b13af
Adam Focht <fochta@asaph.(none)>
2011-01-28 09:26:50 -0500

    Added preliminary client accessor specs.

--- spec/client/client_spec.rb
+++ spec/client/client_spec.rb
@@ -6,4 +6,34 @@ describe "Client" do
       lambda { roc_client = RegonlineConnector::Client.new }.should raise_exception(ArgumentError)
     end
   end
+  
+  describe "with any credentials" do
+    before(:each) do
+      @roc = RegonlineConnector::Client.new(100, 'joeuser', 'password')
+    end
+    
+    it "should not give read access to account_id" do
+      lambda { @roc.account_id }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give read access to username" do
+      lambda { @roc.username }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give read access to password" do
+      lambda { @roc.password }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give write access to account_id" do
+      lambda { @roc.account_id=200 }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give write access to username" do
+      lambda { @roc.username='joeuser' }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give write access to password" do
+      lambda { @roc.password='password' }.should raise_exception(NoMethodError)
+    end
+  end
 end
