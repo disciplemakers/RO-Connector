4aef4bf38cb7c2b0272d2edf33b05734afd0d3fd
Adam Focht <fochta@asaph.(none)>
2011-02-21 17:00:18 -0500

    Factoring and testing for the registration update service method.

--- lib/regonlineconnector/client/registrationupdateservice.rb
+++ lib/regonlineconnector/client/registrationupdateservice.rb
@@ -1,4 +1,4 @@
-require 'soap/wsdlDriver' 
+require 'soap/wsdlDriver'
 
 class RegonlineConnector
   
@@ -9,21 +9,69 @@ class RegonlineConnector
     # RegistrationUpdateService.asmx[https://www.regonline.com/webservices/RegistrationUpdateService.asmx]
     # service.
     class RegistrationUpdateService
-      def initialize( account_id, username, password )
-        @account_id         = account_id
+      def initialize( username, password )
         @username           = username
         @password           = password
-        @wsdl = 'http://www.regonline.com/webservices/RegistrationUpdateService.asmx?WSDL'
-        @registration_updater = SOAP::WSDLDriverFactory.new(@wsdl).create_rpc_driver
       end
       
-      def UpdateRegistrations(updates)
-        response = @registration_updater.UpdateRegistrations(
-                                              {"AccountID" => @account_id,
-                                               "Username"  => @username,
-                                               "Password"  => @password})
-        #response.byAccountIDResult
+      def UpdateRegistrations(request_xml)
+        envelope = SOAP::SOAPEnvelope.new(soap_header, body)
+        request_string = SOAP::Processor.marshal(envelope)
+        request = SOAP::StreamHandler::ConnectionData.new(request_xml)
+        stream = SOAP::HTTPStreamHandler.new(SOAP::Property.new)
+        resp_data = stream.send('http://www.regonline.com/webservices/RegistrationUpdateService.asmx',
+                                request,
+                                "http://www.regonline.com/webservices/2007/08/RegistrationUpdateService/UpdateRegistrations"
+                               )
+      end
+      
+      def generate_request_xml(event_id, update_data_hash)
+        envelope = SOAP::SOAPEnvelope.new(UpdateRegistrationsRequestHeader,
+                                          UpdateRegistrationsRequest(event_id, update_data_hash))
+        request_string = SOAP::Processor.marshal(envelope)
+      end
+      
+      private
+      
+      def UpdateRegistrationsRequestHeader
+        header = SOAP::SOAPHeader.new
+        request_header = SOAP::SOAPElement.new('updateRegistrationsRequestHeader', '')
+        request_header.extraattr['xmlns'] = 'http://www.regonline.com/webservices/2007/08/RegistrationUpdateService'
+        request_header.add(SOAP::SOAPElement.new('login', @username))
+        request_header.add(SOAP::SOAPElement.new('password', @password))
+        header.add(nil, request_header)
+        header
+      end
+      
+      def UpdateRegistrationsRequest(event_id, update_data_hash)
+        body_item = SOAP::SOAPElement.new('UpdateRegistrationsRequest', nil)
+        body_item.extraattr['xmlns'] = 'http://www.regonline.com/webservices/2007/08/RegistrationUpdateService'
+        event_id = SOAP::SOAPElement.new('eventID', '864100')
+        event_id.extraattr['xmlns'] = 'http://www.regonline.com/webservices/2007/08/RegistrationUpdateServiceTypes'
+        body_item.add(event_id)
+        registrations = SOAP::SOAPElement.new('registrations', nil)
+        registrations.extraattr['xmlns'] = 'http://www.regonline.com/webservices/2007/08/RegistrationUpdateServiceTypes'
+        
+        update_data_hash.each do |registration_id, registration_data|
+          registration = SOAP::SOAPElement.new('registration', nil)
+          registration.add(SOAP::SOAPElement.new('registrationId', registration_id))
+          
+          custom_fields = SOAP::SOAPElement.new('customFields', nil)
+          registration_data['customFields'].each do |field_name, field_value|
+            custom_field = SOAP::SOAPElement.new('customField', nil)
+            custom_field.add(SOAP::SOAPElement.new('fieldName', field_name))
+            custom_field.add(SOAP::SOAPElement.new('value', field_value))
+            custom_field.add(SOAP::SOAPElement.new('quantity', '1'))
+            custom_fields.add(custom_field)
+          end
+          registration.add(custom_fields)
+          registrations.add(registration)
+        end
+
+        body_item.add(registrations)
+        body = SOAP::SOAPBody.new(body_item)        
       end
     end
   end
 end
+
--- spec/client/registrationupdateservice/registrationupdateservice_spec.rb
+++ spec/client/registrationupdateservice/registrationupdateservice_spec.rb
@@ -1,9 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "RegOnline" do
+describe "RegistrationUpdateService" do
   describe "without credentials" do
     it "shouldn't instantiate" do
       lambda { roc_rusonline = RegonlineConnector::Client::RegistrationUpdateService.new }.should raise_exception(ArgumentError)
+    end
+  end
+  
+  describe "with any credentials" do
+    before(:each) do
+      resp_data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                  "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                  "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                  "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
+                  "<soap:Body>" +
+                  "<UpdateRegistrationsResponse " +
+                  "xmlns=\"http://www.regonline.com/webservices/2007/08/RegistrationUpdateService\">" +
+                  "<updateRegistrationsResult " +
+                  "xmlns=\"http://www.regonline.com/webservices/2007/08/RegistrationUpdateServiceTypes\">" +
+                  "12345678</updateRegistrationsResult></UpdateRegistrationsResponse></soap:Body>" +
+                  "</soap:Envelope>"
+      mock_http_stream_handler = mock('HTTPStreamHandler')
+      mock_http_stream_handler.stub(:send).with(
+                      'http://www.regonline.com/webservices/RegistrationUpdateService.asmx',
+                      "request",
+                      "http://www.regonline.com/webservices/2007/08/RegistrationUpdateService/UpdateRegistrations"
+                     ).and_return(resp_data)
+      SOAP::HTTPStreamHandler.stub(:new).with(any_args()).and_return(mock_http_stream_handler)      
+      @roc_rus = RegonlineConnector::Client::RegistrationUpdateService.new('joeuser',
+                                                                          'password')
+    end
+    
+    it "should not give read access to username" do
+      lambda { @roc_ro.username }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give read access to password" do
+      lambda { @roc_ro.password }.should raise_exception(NoMethodError)
+    end
+
+    it "should not give write access to username" do
+      lambda { @roc_ro.username='joeuser' }.should raise_exception(NoMethodError)
+    end
+    
+    it "should not give write access to password" do
+      lambda { @roc_ro.password='password' }.should raise_exception(NoMethodError)
     end
   end
 end
